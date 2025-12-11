f6335b47cfa21e911e3ef96796c393c42b75894b
JoaoPedroNeris <jpinheironeris@gmail.com>
2025-12-11 16:53:51 -0300

    80

--- src/app/(auth)/(admin)/admin/sorvetes/page.jsx
+++ src/app/(auth)/(admin)/admin/sorvetes/page.jsx
@@ -1,13 +1,13 @@
 'use client'
 import { SiteContext } from "@/contexts/siteProvider";
 import { API } from "@/services";
-import { Button, Drawer, Form, Input, Popconfirm, Table } from "antd";
+import { Button, Drawer, Form, Input, Popconfirm, Select, Table } from "antd";
 import { useContext, useEffect, useState } from "react";
 import { BiPencil, BiPlus, BiTrash } from "react-icons/bi";
 
-const Niveis = () => {
+const Sorvetes = () => {
 
-    const [niveis, setNiveis] = useState([]);
+    const [sorvetes, setSorvetes] = useState([]);
     const [verCriar, setVerCriar] = useState(false);
     const [verEditar, setVerEditar] = useState(false);
     const [formEditar] = Form.useForm();
@@ -16,8 +16,8 @@ const Niveis = () => {
 
     async function buscar() {
         try {
-            const response = await API.get("/niveis");
-            setNiveis(response.data);
+            const response = await API.get("/sorvetes");
+            setSorvetes(response.data);
         } catch (error) {
             api[error.response.data.tipo]({
                 description: error.response.data.mensagem
@@ -26,8 +26,8 @@ const Niveis = () => {
     }
     async function editar(dados) {
         try {
-            setLoading(true)    
-            const response = await API.put(`/niveis/${dados.id}`, dados);
+            setLoading(true)
+            const response = await API.put(`/sorvetes/${dados.id}`, dados);
             api[response.data.tipo]({
                 description: response.data.mensagem,
                 onClose: () => {
@@ -39,14 +39,14 @@ const Niveis = () => {
             api[error.response.data.tipo]({
                 description: error.response.data.mensagem
             })
-        }finally{
+        } finally {
             setLoading(false)
         }
     }
     async function deletar(id) {
         try {
-            setLoading(true)    
-            const response = await API.delete(`/niveis/${id}`);
+            setLoading(true)
+            const response = await API.delete(`/sorvetes/${id}`);
             api[response.data.tipo]({
                 description: response.data.mensagem,
                 onClose: () => {
@@ -57,14 +57,14 @@ const Niveis = () => {
             api[error.response.data.tipo]({
                 description: error.response.data.mensagem
             })
-        }finally{
+        } finally {
             setLoading(false)
         }
     }
     async function criar(dados) {
         try {
             setLoading(true)
-            const response = await API.post("/niveis", dados);
+            const response = await API.post("/sorvetes", dados);
             api[response.data.tipo]({
                 description: response.data.mensagem,
                 onClose: () => {
@@ -76,7 +76,7 @@ const Niveis = () => {
             api[error.response.data.tipo]({
                 description: error.response.data.mensagem
             })
-        }finally{
+        } finally {
             setLoading(false);
         }
     }
@@ -87,16 +87,16 @@ const Niveis = () => {
     return (
         <div>
             <div className="flex items-center justify-between mb-6">
-                <h6>niveis</h6>
+                <h6>Sorvetes</h6>
                 <Button type="primary"
                     shape="round"
                     icon={<BiPlus />}
                     onClick={() => { setVerCriar(true) }}>
-                    Novo nível
+                    Novo Sabor
                 </Button>
             </div>
             <Table
-                dataSource={niveis || []}
+                dataSource={sorvetes || []}
                 rowKey={"id"}
             >
                 <Table.Column
@@ -109,6 +109,11 @@ const Niveis = () => {
                     title="Nome"
                     dataIndex={"nome"}
                     key={"nome"}
+                />
+                <Table.Column
+                    title="Status"
+                    dataIndex={"status"}
+                    key={"status"}
                 />
                 <Table.Column
                     title="Ações"
@@ -147,6 +152,25 @@ const Niveis = () => {
                         rules={[{ required: true, message: "Campo obrigatorio" }]}>
                         <Input />
                     </Form.Item>
+                    <Form.Item
+                        label="Status"
+                        name="status"
+                        rules={[{ required: true, message: "Campo obrigatorio" }]}
+                    >
+                        <Select
+                            placeholder="Escolha o sabor"
+                            options={[
+                                {
+                                    label: "Ativo",
+                                    value: "Ativo"
+                                },
+                                {
+                                    label: "Inativo",
+                                    value: "inativo"
+                                }
+                            ]}
+                        />
+                    </Form.Item>
                     <Button htmlType="submit"
                         type="primary"
                         loading={loading}
@@ -173,6 +197,25 @@ const Niveis = () => {
                         rules={[{ required: true, message: "Campo obrigatorio" }]}>
                         <Input />
                     </Form.Item>
+                    <Form.Item
+                        label="Status"
+                        name="status"
+                        rules={[{ required: true, message: "Campo obrigatorio" }]}
+                    >
+                        <Select
+                            placeholder="Escolha o sabor"
+                            options={[
+                                {
+                                    label: "Ativo",
+                                    value: "Ativo"
+                                },
+                                {
+                                    label: "Inativo",
+                                    value: "inativo"
+                                }
+                            ]}
+                        />
+                    </Form.Item>
                     <Button htmlType="submit"
                         type="primary"
                         loading={loading}
@@ -183,4 +226,4 @@ const Niveis = () => {
     );
 }
 
-export default Niveis;
+export default Sorvetes;
--- src/contexts/siteProvider.jsx
+++ src/contexts/siteProvider.jsx
@@ -1,6 +1,9 @@
 'use client'
+import { API } from "@/services";
 import { notification } from "antd";
+import { useRouter } from "next/navigation";
 import { createContext } from "react";
+
 
 export const SiteContext = createContext();
 
@@ -10,13 +13,44 @@ const SiteProvider = ({ children }) => {
         showProgress: true,
         duration: 2
     })
+    const router = useRouter();
+    
+    let token = sessionStorage.getItem("token")
+    API.defaults.headers.common.Authorization = `Bearer ${token}`
+    // Adiciona um interceptador na requisição
+    API.interceptors.request.use(function (config) {
+        // Faz alguma coisa antes da requisição ser enviada
+        return config;
+    }, function (error) {
+        // Faz alguma coisa com o erro da requisição
+        if (error.status == 401) {
+            router.push("/login")
+        }
+        return Promise.reject(error);
+    });
+
+    // Adiciona um interceptador na resposta
+    API.interceptors.response.use(function (response) {
+        // Qualquer código de status que dentro do limite de 2xx faz com que está função seja acionada
+        // Faz alguma coisa com os dados de resposta
+        return response;
+    }, function (error) {
+        // Qualquer código de status que não esteja no limite do código 2xx faz com que está função seja acionada
+        // Faz alguma coisa com o erro da resposta
+        if (error.status == 401) {
+            router.push("/login")
+        }
+        return Promise.reject(error);
+        ;
+    })
+
 
     return (
 
-        <SiteContext.Provider value= {{ api }}>
+        <SiteContext.Provider value={{ api }}>
             {contextHolder}
-                {children}
-            
+            {children}
+
         </SiteContext.Provider>
     )
 }
